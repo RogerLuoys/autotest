588fdf9a036117aeaaf93713487f833253c92eb9
luoys <luoys@servyou.com.cn>
2021-04-13 18:59:20 +0800

    新用例

--- src/test/java/testCaseFlag/templateService/UseFlagTemplateTest.java
+++ src/test/java/testCaseFlag/templateService/UseFlagTemplateTest.java
@@ -1,6 +1,10 @@
 package testCaseFlag.templateService;
 
 import com.luoys.upgrade.flag.api.bo.FlagTemplateBO;
+import com.luoys.upgrade.flag.api.enums.FlagStatusEnum;
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import testBase.flag.FlagTestBase;
 
@@ -8,13 +12,24 @@ public class UseFlagTemplateTest extends FlagTestBase {
 
     private final String FullURL = URL + "template/useFlagTemplate";
 
+    @BeforeClass
+    void resetData() {
+        auto.flagDB.delete("delete from flag_bind where flag_id in (select flag_id from flag where flag_name = '自动化脚本转换模板001')");
+        auto.flagDB.delete("delete from flag where flag_name = '自动化脚本转换模板001'");
+    }
+
     @Test
     void test1() {
+        Reporter.log("准备入参");
         FlagTemplateBO flagTemplateBO = new FlagTemplateBO();
-        flagTemplateBO.setFlagName("自动化脚本转换模板");
+        flagTemplateBO.setFlagName("自动化脚本转换模板001");
         flagTemplateBO.setOwnerId("416176799148282");
 
+        Reporter.log("调用接口");
         auto.http.post(FullURL, flagTemplateBO);
 
+        Reporter.log("验证结果");
+        String status = auto.flagDB.selectOneCell("select status from flag where flag_name='自动化脚本转换模板001'");
+        Assert.assertEquals(status, FlagStatusEnum.NOT_START.getCode().toString(), "校验默认状态");
     }
 }
