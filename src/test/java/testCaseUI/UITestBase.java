4c3d551a904730670d8e2fd07392500d6145cbe0
luoys <luoys@servyou.com.cn>
2022-03-01 17:03:12 +0800

    update

--- src/test/java/testCaseUI/UITestBase.java
+++ src/test/java/testCaseUI/UITestBase.java
@@ -9,10 +9,12 @@ import org.testng.annotations.BeforeMethod;
  * 账号userId： 416160586979148
  */
 public class UITestBase {
-    public final static UICommonProxy auto = new UICommonProxy();
+    public UICommonProxy auto;
 
+    // 用例执行时再开始实例化auto变量，因为用mvn test执行会先把test包下所有类实例化一遍
     @BeforeClass(description = "启动浏览器并用自动化专用账号登录")
     public void supperBeforeClass() {
+        auto = new UICommonProxy();
         auto.ui.init(auto.config.URL);
         auto.ui.forceWait(3);
         auto.task.login("UITester", "123456");
