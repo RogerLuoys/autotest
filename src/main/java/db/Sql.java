691def3cb2b2224c56ee9dfdcbe43066a0c1708b
luoys <luoys@servyou.com.cn>
2022-06-02 18:42:33 +0800

    update

---
+++ src/main/java/db/Sql.java
@@ -0,0 +1,30 @@
+package db;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+/**
+ * 试一下
+ */
+public class Sql {
+
+    private DriverManagerDataSource dataSource = null;
+    private final JdbcTemplate jdbcTemplate = new JdbcTemplate();
+
+    public String onePiece(String sql) {
+        this.dataSource = new DriverManagerDataSource();
+        this.dataSource.setDriverClassName("driver");
+        this.dataSource.setUrl("url");
+        this.dataSource.setUsername("userName");
+        this.dataSource.setPassword("password");
+        this.jdbcTemplate.setDataSource(dataSource);
+        jdbcTemplate.update(sql);
+        jdbcTemplate.queryForList(sql);
+        jdbcTemplate.execute(sql);
+        return null;
+    }
+
+    public String runSql(String sql) {
+        return null;
+    }
+}
